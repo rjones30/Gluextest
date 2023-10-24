a93c8957538c0b95ba8bdf9e37855053d2a93240
Richard T. Jones <richard.t.jones@uconn.edu>
2023-10-24 12:23:01 -0400

    * latest updates [rtj]

--- rebuild.sh
+++ rebuild.sh
@@ -1,13 +1,15 @@
 tag=docker.io/rjones30/gluextest:latest
-#docker build --network host --tag $tag .
-docker build --no-cache --network host --tag $tag .
+#podman build --network host --tag $tag .
+podman build --no-cache --network host --tag $tag .
 if [[ $? == 0 ]]; then
-    echo -n "docker build --tag $tag completed successfully, to update the image "
+    echo -n "podman build --tag $tag completed successfully, to update the image "
     echo "on dockerhub you need to follow up with the command below."
-    echo "$ sudo docker push $tag"
+    echo "$ sudo podman push $tag"
+    echo "If you get access denied errors, you need to do the following command first."
+    echo "$ sudo podman login docker.io/rjones30"
     echo "Then if you want to push the update into a local singularity sandbox, do"
-    echo "$ sudo rm -rf out && singularity build --sandbox out docker://registry.hub.docker.com/rjones30/gluex:latest"
+    echo "$ sudo rm -rf out && singularity build --sandbox out docker://registry.hub.docker.com/rjones30/gluextest:latest"
 else
-    echo -n "docker build --tag $tag failed, please fix the problems before you "
+    echo -n "podman build --tag $tag failed, please fix the problems before you "
     echo "push the updates to dockerhub."
 fi
